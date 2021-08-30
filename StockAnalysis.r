bb5c2e790497e395624164f3af490dee0a565911
Phanindra Inabathini <i.phanindra@gmail.com>
2021-08-30 23:06:06 +0100

    Package Import
    First Edit to import packages required for stock analysis

---
+++ StockAnalysis.r
@@ -0,0 +1,12 @@
+# file to analyse stock market data
+#download and import the required packages
+install.packages("tidyquant")
+install.packages("tidyverse")
+install.packages("ggplot2")
+install.packages("ggpubr")
+library(tidyquant)
+library(tidyverse)
+library(ggplot2)
+library(ggpubr)
+
+# import data of all the stocks currently in my portfolio
