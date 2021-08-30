0ec89ff752409b73ec01a277fbfa9a29bfb84b8b
Phanindra Inabathini <i.phanindra@gmail.com>
2021-08-30 23:18:28 +0100

    Tickers and Stock download: initial

--- StockAnalysis.r
+++ StockAnalysis.r
@@ -9,4 +9,12 @@ library(tidyverse)
 library(ggplot2)
 library(ggpubr)
 
+#Downloading data for personal portfolio stocks
+tickers <- c('HSBC','SIVB','SBNY','RY','TFC','USB',
+             'BNS','PNC','EAF','SEB','AL',
+             'LII','GNRC','EXPD','HWM','SPCE','XLI')
+
 # import data of all the stocks currently in my portfolio
+hdfc <- tq_get(c("HDFCBANK.BO"), get = "stock.prices",from = "2021-05-01")
+reliance <- tq_get(c("RELIANCE.NS"), 
+                   get = "stock.prices",from = "2021-05-01") 
