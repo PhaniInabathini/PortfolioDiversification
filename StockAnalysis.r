23301c4ed9f8aa44e3f2dcdfe2155ea0621b2408
Phanindra Inabathini <i.phanindra@gmail.com>
2021-09-15 05:22:11 +0100

    First changes
    These are just initial changes to figure out the path this project is going to take.

--- StockAnalysis.r
+++ StockAnalysis.r
@@ -8,13 +8,25 @@ library(tidyquant)
 library(tidyverse)
 library(ggplot2)
 library(ggpubr)
+library(quantmod)
 
 #Downloading data for personal portfolio stocks
-tickers <- c('HSBC','SIVB','SBNY','RY','TFC','USB',
-             'BNS','PNC','EAF','SEB','AL',
-             'LII','GNRC','EXPD','HWM','SPCE','XLI')
+tickers <- c('HDFCBANK.BO','RELIANCE.NS')
 
 # import data of all the stocks currently in my portfolio
 hdfc <- tq_get(c("HDFCBANK.BO"), get = "stock.prices",from = "2021-05-01")
 reliance <- tq_get(c("RELIANCE.NS"), 
-                   get = "stock.prices",from = "2021-05-01") 
+                   get = "stock.prices",from = "2021-05-01")
+
+View(hdfc)
+hdfc %>% ggplot() + 
+  geom_line(aes(x = date, y = close),color = "darkblue") + 
+  xlab("Year") + ylab("Price")
+
+reliance %>% ggplot() + 
+  geom_line(aes(x = date, y = close),color = "darkblue") + 
+  xlab("Year") + ylab("Price")
+fin.reliance <- tq_get('RELIANCE.NS',get = 'key.ratios')
+# Get key ratios for a stock from Morningstar
+appl_key_ratios <- tq_get("AAPL", get = "financials")
+# as financial data is not available in R directly. Our data will come from python and will be analysed in R.
